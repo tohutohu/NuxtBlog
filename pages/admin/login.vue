34bbcf7b6fb68107488d3f7048690becd7911f14
Ohashi Koya <tohu.soy@gmail.com>
2017-11-29 03:33:28 +0900

    fix: login page design

--- pages/admin/login.vue
+++ pages/admin/login.vue
@@ -1,8 +1,19 @@
 <template>
-<div>
-  <input type="text" name="user" v-model="form.user">
-  <input type="password" name="pass" v-model="form.pass">
-  <input type="submit" @click="login" value="$t('loginButton')">
+<div class="login-page">
+  <div class="login-form">
+    <div class="login-form-item title">Admin Login</div>
+    <div class="login-form-item">
+      <label for="user" class="login-form-label">User:</label>
+      <input type="text" name="user" v-model="form.user">
+    </div>
+    <div class="login-form-item">
+      <label for="pass" class="login-form-label">Pass:</label>
+      <input type="password" name="pass" v-model="form.pass">
+    </div>
+    <div class="login-form-item login-button">
+      <input type="submit" @click="login" value="Login" class="square-button black">
+    </div>
+  </div>
 </div>
 </template>
 
@@ -35,6 +46,32 @@ export default {
 </script>
 
 <style>
+.login-page {
+  display: flex;
+  justify-content: center;
+}
 
+.login-form {
+  margin-top: 48px;
+}
+
+.login-form-item {
+  padding-bottom: 12px;
+  display: flex;
+  justify-content: center;
+}
+
+.login-form-label {
+  margin-right: 6px;
+}
+
+.login-button {
+  padding-top: 6px;
+}
+
+.title {
+  font-size: 36px;
+  padding-bottom: 18px;
+}
 </style>
 
